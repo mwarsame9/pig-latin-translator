d53bd20c47fa983f8a95bc7d746cea64d09820f0
Mohamed Warsame and Sara Schultz <sara@>
2017-03-29 12:02:05 -0700

    add logic with for loop to pass first two specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+//Back-end logic:
+// var userSentenceArray = [userSentence];
+// var vowels = ['a','e','i','o','u']
+// for(var index = 0; index <= userSentenceArray.length; index += 1) {
+//   if (userSentenceArray === vowels[index]) {
+//     result = userSentenceArray + "ay";
+//   }
+// }
+//Front-end logic:
 $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
+
     event.preventDefault();
     var userSentence = $("#sentence").val();
-    $("#result").text(userSentence);
+    var vowels = ['a','e','i','o','u']
+    for(var index = 0; index <= vowels.length; index += 1) {
+      if (userSentence.charAt(0) === vowels[index]) {
+        result = userSentence + "ay";
+      }
+    }
+    console.log(userSentence);
+    $("#result").text(result);
+
   });
-
-
 
 });
